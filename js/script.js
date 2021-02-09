cad01f8cc8871b4de365cd4456b7c612b58885dd
Cristiano <cristiano.tarconi@gmail.com>
2021-02-09 17:23:07 +0100

    genre filter

--- js/script.js
+++ js/script.js
@@ -7,16 +7,50 @@ In base a cosa scegliamo nella select vedremo i corrispondenti cd. */
 let app = new Vue({
     el: '#app',
     data: {
-        disks: []
+        disks: [],
+        genres: ['All'],
+        genSelection: 'All'
     },
     mounted() {
         axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(result => {
+
+                //Estrapolo elementi da API
                 result.data.response.forEach(element => {
+
+                    // Pusho i vari elementi dell'API nell'apposito array creato nel data
                     this.disks.push(element);
+
+                    //Pusho i diversi generi musicali, senza ripetizioni
+                    if (!this.genres.includes(element.genre)) {
+                        this.genres.push(element.genre);
+                    }
+                });
+
+                this.disks.map(element => {
+                    element.visible = true
                 });
             });
 
+    },
+    methods: {
+        filterGen() {
+            
+            if (this.genSelection !== this.genres[0]) {
+                this.disks.forEach(element => {
+                    if (element.genre.toLowerCase() === this.genSelection.toLowerCase()) {
+                        element.visible = true;
+                    } else {
+                        element.visible = false;
+                    }
+                });
+            } else {
+                this.disks.forEach(element => {
+                    element.visible = true;
+                });
+            }
+
+        }
     }
 });
